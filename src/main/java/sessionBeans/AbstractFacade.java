28d9c4ab8d79716b5674eadd490a8d519ac787c6
Douwe Jongeneel <douwejongeneel@gmail.com>
2016-09-29 15:46:03 +0200

    Sessionbean AbstractFacade vanuit PrimeFaces overgezet

--- src/main/java/sessionBeans/AbstractFacade.java
+++ src/main/java/sessionBeans/AbstractFacade.java
@@ -1,64 +1,101 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sessionBeans;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.validation.*;
 
 /**
  *
- * @author douwejongeneel
+ * @author Albert
  */
 public abstract class AbstractFacade<T> {
 
-    private Class<T> entityClass;
+	private Class<T> entityClass;
 
-    public AbstractFacade(Class<T> entityClass) {
-        this.entityClass = entityClass;
-    }
+	public AbstractFacade() {
+	}
 
-    protected abstract EntityManager getEntityManager();
+	public AbstractFacade(Class<T> entityClass) {
+		this.entityClass = entityClass;
+	}
 
-    public void create(T entity) {
-        getEntityManager().persist(entity);
-    }
+	protected abstract EntityManager getEntityManager();
 
-    public void edit(T entity) {
-        getEntityManager().merge(entity);
-    }
+	public T create(T entity) {
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		Validator validator = factory.getValidator();
+		Set<ConstraintViolation<T>> constraintViolations = validator.validate(entity);
+		if (constraintViolations.size() > 0) {
+			Iterator<ConstraintViolation<T>> iterator = constraintViolations.iterator();
+			while (iterator.hasNext()) {
+				ConstraintViolation<T> cv = iterator.next();
+				System.out.println(cv.getRootBeanClass().getName() + "." + cv.getPropertyPath() + " " + cv.getMessage());
+			}
+		} else {
+			getEntityManager().persist(entity);
+			return entity;
+		}
+		return null;
+	}
 
-    public void remove(T entity) {
-        getEntityManager().remove(getEntityManager().merge(entity));
-    }
+	public void edit(T entity) {
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		Validator validator = factory.getValidator();
+		Set<ConstraintViolation<T>> constraintViolations = validator.validate(entity);
+		if (constraintViolations.size() > 0) {
+			Iterator<ConstraintViolation<T>> iterator = constraintViolations.iterator();
+			while (iterator.hasNext()) {
+				ConstraintViolation<T> cv = iterator.next();
+				System.out.println(cv.getRootBeanClass().getName() + "." + cv.getPropertyPath() + " " + cv.getMessage());
+			}
+		} else {
+			getEntityManager().merge(entity);
+		}
+	}
 
-    public T find(Object id) {
-        return getEntityManager().find(entityClass, id);
-    }
+	public void remove(T entity) {
+		getEntityManager().remove(getEntityManager().merge(entity));
+	}
 
-    public List<T> findAll() {
-        javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
-        cq.select(cq.from(entityClass));
-        return getEntityManager().createQuery(cq).getResultList();
-    }
+	public void flush() {
+		getEntityManager().flush();
+	}
 
-    public List<T> findRange(int[] range) {
-        javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
-        cq.select(cq.from(entityClass));
-        javax.persistence.Query q = getEntityManager().createQuery(cq);
-        q.setMaxResults(range[1] - range[0] + 1);
-        q.setFirstResult(range[0]);
-        return q.getResultList();
-    }
+	public T find(Object id) {
+		return getEntityManager().find(entityClass, id);
+	}
 
-    public int count() {
-        javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
-        javax.persistence.criteria.Root<T> rt = cq.from(entityClass);
-        cq.select(getEntityManager().getCriteriaBuilder().count(rt));
-        javax.persistence.Query q = getEntityManager().createQuery(cq);
-        return ((Long) q.getSingleResult()).intValue();
-    }
-    
+	public List<T> withNamedQuery(String namedQuery, String[] parameter, String[] variable) {
+		Query query = getEntityManager().createNamedQuery(namedQuery, entityClass);
+		for (int x = 0; x < parameter.length; x++) {
+			query.setParameter(parameter[x], variable[x]);
+		}
+		return query.getResultList();
+	}
+
+	public List<T> findAll() {
+		javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+		cq.select(cq.from(entityClass));
+		return getEntityManager().createQuery(cq).getResultList();
+	}
+
+	public List<T> findRange(int[] range) {
+		javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+		cq.select(cq.from(entityClass));
+		javax.persistence.Query q = getEntityManager().createQuery(cq);
+		q.setMaxResults(range[1] - range[0] + 1);
+		q.setFirstResult(range[0]);
+		return q.getResultList();
+	}
+
+	public int count() {
+		javax.persistence.criteria.CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+		javax.persistence.criteria.Root<T> rt = cq.from(entityClass);
+		cq.select(getEntityManager().getCriteriaBuilder().count(rt));
+		javax.persistence.Query q = getEntityManager().createQuery(cq);
+		return ((Long) q.getSingleResult()).intValue();
+	}
 }
